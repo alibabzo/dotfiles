bd3fe0961f9f771b8cb2ea2f3f81b2385fdb1601
Alistair Bill <alistair.bill@gmail.com>
2017-03-04 18:12:45 +0000

    Remove unecessary vim plugin definitions

--- cfgs/nvim/plugins.nix
+++ cfgs/nvim/plugins.nix
@@ -2,23 +2,7 @@
 {
   ale = vimUtils.buildVimPluginFrom2Nix {
     name = "ale-1.1.0";
-    src = fetchFromGitHub {
-      owner = "w0rp";
-      repo = "ale";
-      rev = "v1.1.0";
-      sha256 = "0mng29dij25n02qzhsjshw55qbimh0gnawvpjivryq3rf6jkwn3i";
-    };
-    dependencies = [];
-  };
-
-  deoplete-nvim = vimUtils.buildVimPluginFrom2Nix {
-    name = "deoplete-nvim-2.0";
-    src = fetchFromGitHub {
-      owner = "Shougo";
-      repo = "deoplete.nvim";
-      rev = "2.0";
-      sha256 = "1xhn20ybwr45yqnibkc41bpbm87qairib682wgbdr0mwwr3nn8vf";
-    };
+    src = ./../../../Projects/ale;
     dependencies = [];
   };
 
@@ -27,19 +11,19 @@
     src = fetchFromGitHub {
       owner = "Shougo";
       repo = "denite.nvim";
-      rev = "0.1";
-      sha256 = "0c50q2lpsslv4gzmw0w9c2f4x1wf69c4k94i98ynkgl67dxqpl1z";
+      rev = "028ae0c5fa73d63aac831d03b066d670c625cf06";
+      sha256 = "0sfk3f21pqri38jks1casqv2i3lli2gyhpm18rz4jwbw90h4a096";
     };
     dependencies = [];
   };
 
-  vim-colors-pencil = vimUtils.buildVimPluginFrom2Nix {
-    name = "vim-colors-pencil-2017-01-12";
+  nord-vim = vimUtils.buildVimPluginFrom2Nix {
+    name = "nord-vim-2017-02-23";
     src = fetchFromGitHub {
-      owner = "reedes";
-      repo = "vim-colors-pencil";
-      rev = "a28a0b1e778ae394c495ed34efb89d44a05f11f1";
-      sha256 = "15gwyjrwjiqhcpyqp142c0af13ca56zjjnmkhn5ha6mdgikqppbm";
+      owner = "arcticicestudio";
+      repo = "nord-vim";
+      rev = "05d536fe10982612b8b6b0ecef6ee332ae17bc52";
+      sha256 = "1pqcjpr0nmzx35mjk5mj9dg2x0mlws8vw2ggryqar1rnw73vqr8n";
     };
     dependencies = [];
   };
