4a0edffd963e2cb5e2f9386941443135845920fc
Alistair Bill <alistair.bill@gmail.com>
2017-03-27 20:14:06 +0100

    Move init.vim to separate file, update hashes

--- cfgs/nvim/default.nix
+++ cfgs/nvim/default.nix
@@ -1,182 +1,25 @@
 { pkgs }:
 
-let customPlugins = import ./plugins.nix { inherit (pkgs) vimUtils fetchFromGitHub; };
+let
+  customPlugins = import ./plugins.nix { inherit (pkgs) vimUtils fetchFromGitHub; };
+  initvim = builtins.readFile ./init.vim;
 in
 {
-  customRC = ''
-    filetype plugin indent on
-
-    " Full colour mode
-    set termguicolors
-
-    " No beeps/flashes
-    set noerrorbells
-    set novisualbell
-
-    " Don't nag about unwritten changes
-    set hidden
-
-    " Disable annoying messages
-    set noswapfile
-
-    " Prevent lag
-    set lazyredraw
-
-    " Show relative line numbers
-    set relativenumber number
-
-    " Keep 8 screen lines above/below the cursor if possible
-    set scrolloff=8
-
-    " Show current command at the bottom line of screen
-    set showcmd
-    set cmdheight=2
-
-    " Highlight matching bracket
-    set showmatch
-
-    " Always show tabline
-    set showtabline=2
-
-    " Wrap long line, don't break words
-    set wrap linebreak
-
-    " Ignore case when searching
-    set ignorecase
-
-    " ...unless search pattern contains upper-case characters
-    set smartcase
-
-    " Use magic patterns (extended regexps)
-    set magic
-
-    " Use incremental find/replace
-    set incsearch
-    set inccommand=nosplit
-
-    " Show trailing spaces
-    set list listchars=tab:\ \ ,trail:·
-
-    """ Tabs are spaces
-    set expandtab
-    set smarttab
-    set shiftwidth=4
-    set softtabstop=4
-    set tabstop=4
-
-    "Round indent to multiple of 'shiftwidth' when indenting with > and <
-    set shiftround
-
-    " Do smart/autoindenting when starting a new line
-    set smartindent
-    set autoindent
-
-    """ Keymaps
-    " Like C and D, yank from cursor to end of line
-    nnoremap Y y$
-    " Move over wrapped lines the same as normal lines
-    map j gj
-    map k gk
-    " Move to end/beginning of visual (wrapped or normal) line
-    nnoremap & g&
-    nnoremap $ g$
-    nnoremap 0 g0
-    " Copy/paste and move cursor to end of last operated text or end of putted text
-    vnoremap <silent> y y`]
-    vnoremap <silent> p p`]
-    nnoremap <silent> p p`]
-    " Auto center on search match
-    noremap n nzz
-    noremap N Nzz
-    " Use alt-[j,k] to swap lines
-    nmap <M-j> mz:m+<cr>`z
-    nmap <M-k> mz:m-2<cr>`z
-    vmap <M-j> :m'>+<cr>`<my`>mzgv`yo`z
-    vmap <M-k> :m'<-2<cr>`>my`<mzgv`yo`z
-    " Move between windows
-    map <C-j> <C-W>j
-    map <C-k> <C-W>k
-    map <C-h> <C-W>h
-    map <C-l> <C-W>l
-
-    " super-overwrite
-    " from https://wikileaks.org/ciav7p1/cms/page_4849889.html
-    cmap w!! w !sudo tee % > /dev/null
-
-    """ Leader keymaps
-    let mapleader = "\<Space>"
-    let g:mapleader = "\<Space>"
-    nmap <leader>w :w!<cr>
-    map <leader>ba :1,$bd!<cr>
-    map <leader>bd :Bclose<cr>
-    map <leader>tn :tabnew<cr>
-    map <leader>to :tabonly<cr>
-    map <leader>tc :tabclose<cr>
-    map <leader>tm :tabmove
-    map <leader>te :tabedit <c-r>=expand("%:p:h")<cr>/
-
-    """ Auto commands
-    " Toggle hl off when entering insert mode
-    autocmd InsertEnter * :setlocal nohlsearch
-    " Toggle back on when leaving
-    autocmd InsertLeave * :setlocal hlsearch
-    " Delete trailing whitespace
-    autocmd BufWritePre * %s/\s\+$//e
-    " Return to last edit position when opening files
-    autocmd BufReadPost *
-         \ if line("'\"") > 0 && line("'\"") <= line("$") |
-         \   exe "normal! g`\"" |
-         \ endif
-
-    """ Abbreviations
-    " Common mistakes when typing commandss
-    cnoreabbrev W w
-    cnoreabbrev Q q
-    cnoreabbrev Wq wq
-    cnoreabbrev WQ wq
-    cnoreabbrev Tabe tabe
-
-    """ Plugins
-    " Airline
-    set noshowmode
-    let g:airline#extensions#tabline#enabled = 1
-    let g:airline_powerline_fonts = 1
-    let g:airline_theme='nord'
-
-    " Deoplete
-    let g:deoplete#enable_at_startup = 1
-    autocmd CompleteDone * pclose
-    inoremap <expr><tab> pumvisible() ? "\<c-n>" : "\<tab>"
-
-    " Colourscheme
-    if filereadable(expand('~').'/.config/colourscheme')
-      let scheme = readfile(expand('~').'/.config/colourscheme')
-      if scheme == ['light']
-        colorscheme one
-        set background=light
-      else
-        colorscheme nord
-      endif
-    endif
-
-    " Enable syntax highlighting
-    syntax enable
-  '';
+  customRC = initvim;
 
   vam.knownPlugins = pkgs.vimPlugins // customPlugins;
-
   vam.pluginDictionaries = [
-    {
-      names = [
-        "ale"
-        "deoplete-nvim"
-        "denite-nvim"
-        "vim-airline"
-        "vim-nix"
-        "nord-vim"
-        "vim-one"
-        "The_NERD_tree"
-      ];
-    }
+    "ale"
+    "deoplete-nvim"
+    "neco-ghc"
+    "neosnippet-vim"
+    "neosnippet-snippets"
+    "nord-vim"
+    "Supertab"
+    "The_NERD_tree"
+    "vim-airline"
+    "vim-gitgutter"
+    "vim-nix"
+    "vim-one"
   ];
 }
--- cfgs/nvim/plugins.nix
+++ cfgs/nvim/plugins.nix
@@ -1,44 +1,37 @@
 { vimUtils, fetchFromGitHub }:
+let buildVimPlugin = vimUtils.buildVimPluginFrom2Nix;
+in
 {
-  ale = vimUtils.buildVimPluginFrom2Nix {
-    name = "ale-2017-03-06";
+  ale = buildVimPlugin {
+    name = "ale-2017-03-24";
     src = fetchFromGitHub {
       owner = "w0rp";
       repo = "ale";
-      rev = "ad49846a48beab2ed7fe58a799e6a53e99086bb2";
-      sha256 = "0n2bz6icirdx39z7mxxccmxj6fzn3pa96k8lnp4ph295haszvlb0";
-    };
-  };
-
-  denite-nvim = vimUtils.buildVimPluginFrom2Nix {
-    name = "denite-nvim-0.1";
-    src = fetchFromGitHub {
-      owner = "Shougo";
-      repo = "denite.nvim";
-      rev = "f2a1f5c89ca093ca286bfaef9e3d53ecb045390e";
-      sha256 = "0wvsd91afvrg1rykc9bnmcr38wlz2iz9w8df2b49hvp6zfpq29hr";
+      rev = "822b19ac8376677d73c4df5ce80ed708a1cabd8b";
+      sha256 = "01942mjn1p7hyn1y2jlmrzqfwzdmnkcvqjw654iwjirnawiz5cj2";
     };
     dependencies = [];
   };
 
-  nord-vim = vimUtils.buildVimPluginFrom2Nix {
+  nord-vim = buildVimPlugin {
     name = "nord-vim-2017-02-23";
     src = fetchFromGitHub {
       owner = "arcticicestudio";
       repo = "nord-vim";
-      rev = "05d536fe10982612b8b6b0ecef6ee332ae17bc52";
-      sha256 = "1pqcjpr0nmzx35mjk5mj9dg2x0mlws8vw2ggryqar1rnw73vqr8n";
+      rev = "b2134029a0481c4bbb6d0cf586948afbc65818de";
+      sha256 = "0ddbc78n71hdmg9fca0cb3iyx7jxs6am1lgzf71srr6r7x8y9fhz";
     };
     dependencies = [];
   };
 
-  vim-one = vimUtils.buildVimPluginFrom2Nix {
+  vim-one = buildVimPlugin {
     name = "vim-one-2017-03-02";
     src = fetchFromGitHub {
       owner = "rakr";
       repo = "vim-one";
-      rev = "ccd2d3c6ee00a1de84016bca506d2cf93048c96e";
-      sha256 = "156mhbld92gnvz99d5ns5dzzs2ak3n3501nz0g0sr8nk497c59zc";
+      rev = "ccef53630e9db0acd5e96521b926a9b5ad657867";
+      sha256 = "16xaxdn7kniqmdbs545450qak4gygl4lsdvx7jjf4wrdpy9178qf";
     };
+    dependencies = [];
   };
 }
