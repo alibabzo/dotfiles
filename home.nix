30617f22eefc3e7d3c77854b25930ee5436bfe76
Alistair Bill <alistair.bill@gmail.com>
2017-04-24 21:31:24 +0100

    Use nix-shell shebang to include dependencies with scripts

--- home.nix
+++ home.nix
@@ -1,14 +1,22 @@
-{ pkgs, ... }:
-with import <nixpkgs> { };
+{ pkgs, lib, ...}:
 let
-  script = source: {
-    inherit source;
-    mode = "0755";
-  };
+  /*
+  From
+  { file = "regular"; file2 = "directory"; }
+  to
+  { "location/file".source = path/file; }
+  */
+  getFiles = location: path:
+    lib.mapAttrs' (name: value: lib.nameValuePair (location + ("/" + name))
+    ({ source = (path + ("/" + name)); })) (builtins.readDir path);
+
+  getScriptFiles = location: path:
+    lib.mapAttrs' (name: value: lib.nameValuePair (location + ("/" + name))
+    ({ source = (path + ("/" + name)); mode = "0755"; })) (builtins.readDir path);
 
   fisherman = pkgs.stdenv.mkDerivation {
     name = "fisherman";
-    src = fetchFromGitHub {
+    src = pkgs.fetchFromGitHub {
       rev = "91f447ec4087635ea988e6864288cb73a0aba504";
       repo = "fisherman";
       owner = "fisherman";
@@ -19,35 +27,21 @@ let
       cp -a . $out
     '';
   };
-
 in
 {
   home.packages = with pkgs; [
     acpi
-    bc
     chromium
     coreutils
-    emacs
-    feh
     font-awesome-ttf
-    ghc
     gimp-with-plugins
-    git
     hexchat
-    haskellPackages.hindent
-    haskellPackages.hlint
     haskellPackages.stack-run
     htop
-    imagemagick
-    jq
     libnotify
     lxappearance
-    mono
-    msgpack-tools
-    ncmpcpp
     neofetch
     neovim
-    netcat-openbsd
     nitrogen
     nox
     pavucontrol
@@ -56,22 +50,11 @@ in
     roboto
     rofi
     scrot
-    shellcheck
     stack
     termite
-    unzip
-    vim-vint
     vlc
-    weechat
-    wirelesstools
     xarchiver
     xclip
-    xdotool
-    xorg.xev
-    xorg.xinput
-    xorg.xkbcomp
-    xorg.xmessage
-    xorg.xset
     zip
   ];
 
@@ -81,61 +64,11 @@ in
     userEmail = "alistair.bill@gmail.com";
   };
 
-  home.file = {
-    # scripts
-    "bin/colour-scheme" = script ./cfgs/bin/colour-scheme;
-    "bin/init_keyboard" = script ./cfgs/bin/init_keyboard;
-    "bin/shutdown_menu" = script ./cfgs/bin/shutdown_menu;
-
-    # xmobar
-    ".config/xmobar/scripts/network.sh" = script ./cfgs/xmobar/scripts/network.sh;
-    ".config/xmobar/scripts/load.sh" = script ./cfgs/xmobar/scripts/load.sh;
-    ".config/xmobar/scripts/volume.sh" = script ./cfgs/xmobar/scripts/volume.sh;
-    ".config/xmobar/scripts/battery.sh" = script ./cfgs/xmobar/scripts/battery.sh;
-    ".config/xmobar/xmobarrc".source = ./cfgs/xmobar/xmobarrc;
-
-    # xmonad
-    ".config/xmonad/xmonad.hs".source = ./cfgs/xmonad/xmonad.hs;
-    ".config/xmonad/lib/XMobar.hs".source = ./cfgs/xmonad/lib/XMobar.hs;
-
-    # misc
-    ".config/compton.conf".source = ./cfgs/compton/compton.conf;
-    ".config/rofi/launcher".source = ./cfgs/rofi/launcher;
-    ".config/termite/config-dark".source = ./cfgs/termite/config-dark;
-    ".config/termite/config-light".source = ./cfgs/termite/config-light;
-    ".xkb/symbols/poker".source = ./cfgs/xkb/poker;
-
-    # fish
-    ".config/fish/conf.d/aliases.fish".source = ./cfgs/fish/conf.d/aliases.fish;
-    ".config/fish/conf.d/prompt.fish".source = ./cfgs/fish/conf.d/prompt.fish;
-    ".config/fish/conf.d/vimode.fish".source = ./cfgs/fish/conf.d/vimode.fish;
-    ".config/fish/functions/cd.fish".source = ./cfgs/fish/functions/cd.fish;
-    ".config/fish/functions/extract.fish".source = ./cfgs/fish/functions/extract.fish;
-    ".config/fish/functions/fisher.fish".source = "${fisherman}/fisher.fish";
-    ".config/fish/fishfile".source = ./cfgs/fish/fishfile;
-  };
-
   gtk = {
     enable = true;
     fontName = "Roboto 10";
     iconThemeName = "Paper";
     themeName = "Arc-Dark";
-
-    gtk2.extraConfig = ''
-      gtk-xft-antialias=1
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle="hintslight"
-      gtk-xft-rgba="rgb"
-    '';
-
-    gtk3.extraConfig = {
-      gtk-cursor-theme-size = 16;
-      gtk-cursor-theme-name = "Paper";
-      gtk-xft-antialias = 1;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintslight";
-      gtk-xft-rgba = "rgb";
-    };
 
     gtk3.extraCss = ''
       VteTerminal, vte-terminal {
@@ -143,4 +76,24 @@ in
       }
     '';
   };
+
+  home.file =
+  {
+    ".config/xmobar/xmobarrc".source = ./cfgs/xmobar/xmobarrc;
+
+    ".config/xmonad/xmonad.hs".source = ./cfgs/xmonad/xmonad.hs;
+    ".config/xmonad/lib/XMobar.hs".source = ./cfgs/xmonad/lib/XMobar.hs;
+
+
+    ".config/fish/functions/fisher.fish".source = "${fisherman}/fisher.fish";
+    ".config/fish/fishfile".source = ./cfgs/fish/fishfile;
+
+    ".config/xkb/symbols/poker".source = ./cfgs/xkb/poker;
+  }
+  // getFiles ".config/fish/functions" ./cfgs/fish/functions
+  // getFiles ".config/fish/conf.d" ./cfgs/fish/conf.d
+  // getFiles ".config/termite" ./cfgs/termite
+  // getScriptFiles ".config/xmobar/scripts" ./cfgs/xmobar/scripts
+  // getScriptFiles "bin" ./cfgs/bin;
 }
+
