947b68717d4540cff1ea7d1d5fb0b51f5b7e2824
Alistair Bill <alistair.bill@gmail.com>
2018-03-01 18:35:22 +0000

    Update polybar config

---
+++ .config/polybar/scripts/archpkg.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+arch_updates=$(checkupdates 2> /dev/null | wc -l)
+aur_updates=$(trizen -u 2> /dev/null | wc -l)
+
+if [ "$arch_updates" -gt 0 ] || [ "$aur_updates" -gt 0 ]; then
+        echo $((arch_updates + aur_updates))
+else
+        echo "";
+fi
