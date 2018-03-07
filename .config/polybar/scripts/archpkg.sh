ab64599e64fbefab74141ceee410919d0bb7629c
Alistair Bill <alistair.bill@gmail.com>
2018-03-07 13:33:16 +0000

    Fix archpkg script

--- .config/polybar/scripts/archpkg.sh
+++ .config/polybar/scripts/archpkg.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 arch_updates=$(checkupdates 2> /dev/null | wc -l)
-aur_updates=$(trizen -u 2> /dev/null | wc -l)
+aur_updates=$(trizen -Qu --aur 2> /dev/null | wc -l)
 
 if [ "$arch_updates" -gt 0 ] || [ "$aur_updates" -gt 0 ]; then
         echo $((arch_updates + aur_updates))
